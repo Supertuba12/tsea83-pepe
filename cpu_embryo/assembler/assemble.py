dea659e3f1d3e6ed3e6bb03b8e5d332e09198b62
Henrik Andersson <he.and03a@gmail.com>
2017-04-04 16:59:36 +0200

    Assembler 1.0 completed

---
+++ cpu_embryo/assembler/assemble.py
@@ -0,0 +1,84 @@
+"""
+    ~ Assembler for TSEA83 project ~
+    Run with: python3 -c 'from assemble import *; assemble("example", "settings")'
+
+    Output implemntation in opcalls.py
+"""
+import opcalls
+
+def assemble(assembly, settings):
+    """ Run assemble with your assembly code as arg0 settings as arg1 """
+    settings = read_settings(settings)
+    opcodes = settings[0]
+    adrmodes = settings[1]
+    output_file = open('code', 'w+')
+
+    subr = {}
+    adr = [0]
+
+    with open(assembly) as asm_file:
+        for line in asm_file:
+            line = line.split()
+            if not line or ";" in line[0]:
+                continue
+
+            if not line[0] in opcodes:
+                subr[line[0]] = adr[0]
+                if len(line) > 0:
+                    line.remove(line[0])
+                else:
+                    break
+
+            print("OPCODE: ", line[0:2])
+            output = ""
+            if len(line) == 1: # HALT
+                output = (handler(line[0]))(opcodes)
+            elif "," not in line[1]: # JMP, BGE, BEQ etc
+                output = (handler(line[0]))(line[1:2], opcodes, subr)
+            else: # STORE, LOAD, ADD, SUB etc
+                output = (handler(line[0]))(line[1:2], opcodes, adrmodes, adr)
+
+            output_file.write(output + "\n")
+
+            adr[0] += 1
+
+    output_file.close()
+
+
+def handler(opcode):
+    """ Python with switch """
+    return {
+        'LOAD': opcalls.load,
+        'STORE': opcalls.store,
+        'ADD': opcalls.add,
+        'SUB': opcalls.sub,
+        'CMP': opcalls.op_cmp,
+        'BGE': opcalls.bge,
+        'AND': opcalls.op_and,
+        'HALT': opcalls.halt,
+        'JMP': opcalls.jmp,
+    }[opcode]
+
+
+def read_settings(settings):
+    """ Reads the settings file. """
+    with open(settings) as conf_file:
+        for line in conf_file:
+            if "OP" in line:
+                opcodes = unpack(conf_file)
+            if "MODES" in line:
+                adrmodes = unpack(conf_file)
+
+    return (opcodes, adrmodes)
+
+
+def unpack(file):
+    """ Unpacks each section of settings. """
+    unpacked_values = {}
+    for line in file:
+        if "\n" in line[0]:
+            break
+        line = line.split(",")
+        unpacked_values[line[0]] = line[1].rstrip()
+
+    return unpacked_values
---
+++ cpu_embryo/assembler/opcalls.py
@@ -0,0 +1,104 @@
+""" Commented output for debugging """
+COMMENTS = True
+
+def eval_args(line, adrmodes, adr):
+    """ Evaluates adress mode and GRx memory """
+    direct = False
+
+    if line[0] == "GR0":
+        grx = 0
+    else:
+        grx = 0b100000000000
+
+    if line[1][0] == "#":
+        adrmode = int(adrmodes['DIRECT'])
+        adr_pos = adr[0] + 1
+        direct = True
+        adr[0] += 1
+    else:
+        adrmode = int(adrmodes['ABSOLUTE'])
+        adr_pos = int(line[1][1:], 16)
+
+    if adrmode == 1:
+        adrmode_bin = 0b010000000000
+    else:
+        adrmode_bin = 0
+
+    return (direct, format((grx + adrmode_bin + adr_pos), '03x'))
+
+
+def single_arg_op(line, opcodes, subr, opc):
+    """ Implementations of all jumps are the same. """
+    adr_pos = format(subr[line[0]], '03x')
+    if COMMENTS:
+        return opcodes[opc] + adr_pos + "; " + opc
+    else:
+        return opcodes[opc] + adr_pos
+
+
+def double_arg_op(line, opcodes, adrmodes, adr, opc):
+    """ The implementation of the basic version of all dual argument operations
+        is the same. """
+    line = line[0].split(',')
+    arg_result = eval_args(line, adrmodes, adr)
+    direct = arg_result[0]
+    tail_num = arg_result[1]
+
+    if COMMENTS:
+        if direct:
+            return opcodes[opc] + tail_num + "; " + opc + "\n" + format(int(line[1][1:], 16), '04x')
+        else:
+            return opcodes[opc] + tail_num + "; " + opc
+    else:
+        if direct:
+            return opcodes[opc] + tail_num + "\n" + format(int(line[1][1:], 16), '04x')
+        else:
+            return opcodes[opc] + tail_num
+
+
+def load(line, opcodes, adrmodes, adr):
+    """ ~ Standard LOAD ~ GRx,(#X/$Y) """
+    return double_arg_op(line, opcodes, adrmodes, adr, 'LOAD')
+
+
+def store(line, opcodes, adrmodes, adr):
+    """ ~ Standard STORE ~ GRx,(#X/$Y) """
+    return double_arg_op(line, opcodes, adrmodes, adr, 'STORE')
+
+
+def add(line, opcodes, adrmodes, adr):
+    """ ~ Standard ADD ~ GRx,(#X/$Y) """
+    return double_arg_op(line, opcodes, adrmodes, adr, 'ADD')
+
+
+def sub(line, opcodes, adrmodes, adr):
+    """ ~ Standard SUB ~ GRx,(#X/$Y) """
+    return double_arg_op(line, opcodes, adrmodes, adr, 'SUB')
+
+
+def op_cmp(line, opcodes, adrmodes, adr):
+    """ ~ Standard CMP ~ GRx,(#X/$Y) """
+    return double_arg_op(line, opcodes, adrmodes, adr, 'CMP')
+
+
+def bge(line, opcodes, subr):
+    """ ~ Standard BGE ~ SUBROUTINE """
+    return single_arg_op(line, opcodes, subr, 'BGE')
+
+
+def op_and(line, opcodes, adrmodes, adr):
+    """ ~ Standard CMP ~ GRx,(#X/$Y) """
+    return double_arg_op(line, opcodes, adrmodes, adr, 'AND')
+
+
+def halt(opcodes):
+    """ ~ HALT ~ """
+    if COMMENTS:
+        return opcodes['HALT'] + "000; HALT"
+    else:
+        return opcodes['HALT'] + "000"
+
+
+def jmp(line, opcodes, subr):
+    """ ~ Standard JMP ~ SUBROUTINE """
+    return single_arg_op(line, opcodes, subr, 'JMP')
