03fc9e0d9a6c9812bd7b4d041070e29d5b7eb149
Henrik Andersson <he.and03a@gmail.com>
2017-04-06 12:50:51 +0200

    Load implemented

--- cpu_embryo/assembler/assemble.py
+++ cpu_embryo/assembler/assemble.py
@@ -19,9 +19,58 @@ def assemble(assembly, settings):
                 else:
                     break
 
-            print("OPCODE ", line[0])
+            print("OPCODE: ", line)
+            output = ""
+            if len(line) > 1:
+                output = (handler(line[0]))(line[1:], opcodes, adrmodes, adr)
+            else:
+                output = (handler(line[0]))(opcodes)
+            print(output)
+            #print(format(int(output, 16), '016b'))
+
+            adr += 1
 
 
+def handler(opcode):
+    return {
+        'LOAD': load,
+        'STORE': store,
+        'HALT': halt,
+    }[opcode]
+
+
+def halt(opcodes):
+    return opcodes['HALT'] + "000"
+
+
+
+def load(line, opcodes, adrmodes, adr):
+    line = line[0].split(',')
+
+    if line[0] == "GR0":
+        grx = 0
+    else:
+        grx = 0b100000000000
+
+    if line[1][0] == "#":
+        adrmode = int(adrmodes['DIRECT'])
+        adr_pos = adr + 1
+    else:
+        adrmode = int(adrmodes['ABSOLUTE'])
+        adr_pos = int(line[1][1:], 16)
+
+    if adrmode == 1:
+        adrmode_bin = 0b010000000000
+    else:
+        adrmode_bin = 0
+
+    tail_num = format((grx + adrmode_bin + adr_pos), '03x')
+
+    return opcodes['LOAD'] + str(tail_num)
+
+
+def store(line):
+    pass
 
 def read_settings(settings):
     with open(settings) as conf_file:
