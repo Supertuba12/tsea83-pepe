c8f230bd551deece36860e21d37e002bfe07d589
Hampus Eriksson <hamer848@student.liu.se>
2017-04-06 15:01:39 +0200

    edited cpu

--- cpu_embryo/proj.vhd
+++ cpu_embryo/proj.vhd
@@ -12,9 +12,9 @@ architecture Behavioral of proj is
 
 
   -- micro Memory component
-  component uMem
+  component em
     port(uAddr : in unsigned(5 downto 0);
-         uData : out unsigned(15 downto 0));
+         uData : out unsigned(19 downto 0));
   end component;
 
   -- program Memory component
@@ -24,13 +24,15 @@ architecture Behavioral of proj is
   end component;
   
   -- micro memory signals
-  signal uM : unsigned(15 downto 0); -- micro Memory output
+  signal uM : unsigned(19 downto 0); -- micro Memory output
   signal uPC : unsigned(5 downto 0); -- micro Program Counter
-  signal uPCsig : std_logic; -- (0:uPC++, 1:uPC=uAddr)
+  signal SEQ : unsigned(3 downto 0); -- uPC controller
   signal uAddr : unsigned(5 downto 0); -- micro Address
   signal TB : unsigned(2 downto 0); -- To Bus field
   signal FB : unsigned(2 downto 0); -- From Bus field
-	
+	signal ALU : unsigned(2 downto 0); -- ALU operand
+  signal K1 : unsigned (7 downto 0); -- K1 signal
+  signal K2 : unsigned (7 downto 0); -- K2 signal
   -- program memory signals
   signal PM : unsigned(15 downto 0); -- Program Memory output
   signal PC : unsigned(15 downto 0); -- Program Counter
@@ -38,7 +40,16 @@ architecture Behavioral of proj is
   signal ASR : unsigned(15 downto 0); -- Address Register
   signal IR : unsigned(15 downto 0); -- Instruction Register
   signal DATA_BUS : unsigned(15 downto 0); -- Data Bus
-	
+  -- Registers
+  signal AR : unsigned(15 downto 0); -- AR register for ALU
+	signal HELP_REG : unsigned(15 downto 0); -- Help register
+  signal GR0 : unsigned(15 downto 0); -- General-use register 
+  signal GR1 : unsigned(15 downto 0); -- General-use register
+  signal GRX : unsigned(15 downto 0); -- Bus signal for chosen G-register
+  -- Flags
+  signal Z : std_logic; -- Z = 1 if value @ AR == 0 else N = 0
+  signal N : std_logic; -- N = 1 if value @ AR < 0 else N = 0
+  signal O : std_logic; -- O = 1 if operation in ALU caused overflow
 
 begin
 
@@ -48,7 +59,7 @@ begin
     if rising_edge(clk) then
       if (rst = '1') then
         uPC <= (others => '0');
-      elsif (uPCsig = '1') then
+      elsif (SEQ = '') then
         uPC <= uAddr;
       else
         uPC <= uPC + 1;
@@ -100,42 +111,74 @@ begin
     if rising_edge(clk) then
       if (rst = '1') then
         -- Maybe reset AR?
-      elsif () then
-        
       end if;
+      case SEQ is
+        when "001" => -- AR := BUSS
+          AR <= DATA_BUS;
+        when "010" => -- Undef. Could be set to what we want
+
+        when "011" => -- AR := 0
+          AR <= 0;
+        when "100" => -- AR := AR + BUSS
+          AR <= AR + DATA_BUS;
+        when "101" => -- AR := AR - BUSS
+          AR <= AR - DATA_BUS
+        when "110" => -- AR := AR && BUSS
+          AR <= AR and DATA_BUS;
+        when "111" => -- AR := AR || BUSS
+          AR <= AR or DATA_BUS;
+        when others => -- NOP
+          null;
+      end case;
     end if;
-  end process; 
+  end process;  
 
   -- General registers 
 	process(clk)
   begin
     if rising_edge(clk) then
       if (rst = '1') then
-        -- Reset all registers?
-      elsif () then
-
+        -- Reset all registers? 
       end if;
     end if;
   end process;
 
+  -- K1 memory
+  with ALU select K1 <=
+    to_unsigned(X"00") when "0000"
+    to_unsigned(X"00") when "0001"
+    to_unsigned(X"00") when "0010"
+    to_unsigned(X"00") when "0011"
+    to_unsigned(X"00") when "0100"
+    to_unsigned(X"00") when "0101"
+    to_unsigned(X"00") when "0110"
+    to_unsigned(X"00") when "0111"
+  
+  -- K2 assignment
+
+
   -- micro memory component connection
-  U0 : uMem port map(uAddr=>uPC, uData=>uM);
+  U0 : em port map(uAddr=>uPC, uData=>uM);
 
   -- program memory component connection
   U1 : pMem port map(pAddr=>ASR, pData=>PM);
 	
   -- micro memory signal assignments
-  uAddr <= uM(5 downto 0);
-  uPCsig <= uM(6);
-  PCsig <= uM(7);
-  FB <= uM(10 downto 8);
-  TB <= uM(13 downto 11);
+  uAddr <= (5 downto 0);
+  SEQ <= (9 downto 6);
+  PCsig <= (10);
+  FB <= (13 downto 11);
+  TB <= (16 downto 14);
+  ALU <= (19 downto 17)
 	
   -- data bus assignment
   DATA_BUS <= IR when (TB = "001") else
     PM when (TB = "010") else
     PC when (TB = "011") else
     ASR when (TB = "100") else
-   (others => '0');
+    AR when (TB = "101") else
+    HELP_REG when (TB = "110") else
+    GRX when (TB = "111")
+    (others => '0');
 
 end Behavioral;
