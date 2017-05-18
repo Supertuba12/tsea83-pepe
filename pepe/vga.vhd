3f2471a9bb846ab631b7dae5c7a842af937a6c77
Henrik Andersson <henan076@student.liu.se>
2017-05-18 21:19:02 +0200

    Only highscore bigger than the top are recorded, but it works

--- pepe/vga.vhd
+++ pepe/vga.vhd
@@ -54,8 +54,6 @@ architecture Behavioral of VGA is
   signal  Ypepe           : unsigned(9 downto 0)          := "0111000000";
   signal  counter         : unsigned(19 downto 0)         := to_unsigned(0, 20);
   signal  index_h         : unsigned(4 downto 0)          := to_unsigned(0, 5);
-  signal  coll            : std_logic                     := '0';
-  signal  done            : std_logic                     := '0';
   signal  home_pre        : unsigned(3 downto 0)          := to_unsigned(0, 4);
   signal  score_out_s     : unsigned(15 downto 0)         := to_unsigned(0, 16);
   signal  score_num_id    : unsigned(15 downto 0)         := to_unsigned(0, 16);
@@ -63,13 +61,15 @@ architecture Behavioral of VGA is
   signal  dead            : std_logic                     := '0';
   signal  tile_index_s    : unsigned(4 downto 0)          := to_unsigned(0, 5);
   signal  tile_index_d    : unsigned(4 downto 0)          := to_unsigned(0, 5);
-
-  type lut_t is array (0 to 11) of unsigned(3 downto 0);
-
+  signal bajs             : std_logic;
+  
+  type lut_t is array (11 downto 0) of unsigned(3 downto 0);
+  type lut_2d is array (4 downto 0) of lut_t;
   constant lut_c : lut_t := (others => "0000");
 
   signal lut : lut_t := lut_c;
   signal index_save : lut_t := lut_c;
+  signal highscore_lut : lut_2d := (others => lut_c);
 
   component ram
     port (clk       : in std_logic;
@@ -91,7 +91,7 @@ architecture Behavioral of VGA is
       x                     : in unsigned(6 downto 0); -- Range 0 - 15
       y                     : in unsigned(6 downto 0); -- Range 0 - 15
       tileIndex             : in unsigned(4 downto 0); -- Range 0 - 17
-      data_out              : out std_logic_vector(7 downto 0));  
+      data_out              : out std_logic_vector(7 downto 0));
     end component;
 begin
   -- Clock divisor
@@ -141,7 +141,6 @@ begin
     if rising_edge(clk) then
       if rst = '1' then
         rng <= (others => '0');
-        index_save <= lut_c;
         home_cp <= (others => '0');
         start_y_p_cp <= (others => '0');
         y_tile_cp <= (others => '0');
@@ -237,11 +236,69 @@ begin
   x_p <= xtile;
   x_out <= Xpixel - Xpepe;
   y_out <= Ypixel - Ypepe;
-  x_h <= "000" & Xpixel(3 downto 0);
-  y_h <= Ypixel(6 downto 0);
-  score_num_id <= to_unsigned(0, 11) & (Xpixel(8 downto 4) - 8);
-  index_h <= (Xpixel(8 downto 4) + 10) when (Xpixel < 144) else ("0" & index_save(to_integer(score_num_id)));
   tile_index <= tile_index_s when (dead = '0') else tile_index_d;
+  
+  x_h <= "000" & Xpixel(3 downto 0);
+  y_h <= "000" & Ypixel(3 downto 0);
+  --index_h <= (Xpixel(8 downto 4) + 10) when (Xpixel < 144) else ("0" & index_save(to_integer(score_num_id)));
+  index_h <=
+      to_unsigned(14, 5)             when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 0 else
+      to_unsigned(15, 5)             when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 0 else
+      to_unsigned(16, 5)             when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 0 else
+      to_unsigned(17, 5)             when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 0 else
+      to_unsigned(18, 5)             when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 0 else
+      "0" & index_save(1)            when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 1 else
+      "0" & index_save(2)            when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 1 else
+      "0" & index_save(3)            when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 1 else
+      "0" & index_save(4)            when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 1 else
+      "0" & index_save(5)            when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 1 else
+      "0" & index_save(6)            when Xpixel(8 downto 4) = 10 and Ypixel(8 downto 4) = 1 else
+      to_unsigned(10, 5)             when Xpixel(8 downto 4) = 3 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(11, 5)             when Xpixel(8 downto 4) = 4 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(12, 5)             when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(13, 5)             when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(14, 5)             when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(15, 5)             when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(16, 5)             when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(17, 5)             when Xpixel(8 downto 4) = 10 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(18, 5)             when Xpixel(8 downto 4) = 11 and Ypixel(8 downto 4) = 3 else
+      to_unsigned(1, 5)              when Xpixel(8 downto 4) = 2 and Ypixel(8 downto 4) = 5 else
+      "0" & highscore_lut(0)(1)      when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 5 else
+      "0" & highscore_lut(0)(2)      when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 5 else
+      "0" & highscore_lut(0)(3)      when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 5 else
+      "0" & highscore_lut(0)(4)      when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 5 else
+      "0" & highscore_lut(0)(5)      when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 5 else
+      "0" & highscore_lut(0)(6)      when Xpixel(8 downto 4) = 10 and Ypixel(8 downto 4) = 5 else
+      to_unsigned(2, 5)              when Xpixel(8 downto 4) = 2 and Ypixel(8 downto 4) = 6 else
+      "0" & highscore_lut(1)(1)      when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 6 else
+      "0" & highscore_lut(1)(2)      when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 6 else
+      "0" & highscore_lut(1)(3)      when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 6 else
+      "0" & highscore_lut(1)(4)      when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 6 else
+      "0" & highscore_lut(1)(5)      when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 6 else
+      "0" & highscore_lut(1)(6)      when Xpixel(8 downto 4) = 10 and Ypixel(8 downto 4) = 6 else
+      to_unsigned(3, 5)              when Xpixel(8 downto 4) = 2 and Ypixel(8 downto 4) = 7 else
+      "0" & highscore_lut(2)(1)      when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 7 else
+      "0" & highscore_lut(2)(2)      when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 7 else
+      "0" & highscore_lut(2)(3)      when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 7 else
+      "0" & highscore_lut(2)(4)      when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 7 else
+      "0" & highscore_lut(2)(5)      when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 7 else
+      "0" & highscore_lut(2)(6)      when Xpixel(8 downto 4) = 10 and Ypixel(8 downto 4) = 7 else
+      to_unsigned(4, 5)              when Xpixel(8 downto 4) = 2 and Ypixel(8 downto 4) = 8 else
+      "0" & highscore_lut(3)(1)      when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 8 else
+      "0" & highscore_lut(3)(2)      when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 8 else
+      "0" & highscore_lut(3)(3)      when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 8 else
+      "0" & highscore_lut(3)(4)      when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 8 else
+      "0" & highscore_lut(3)(5)      when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 8 else
+      "0" & highscore_lut(3)(6)      when Xpixel(8 downto 4) = 10 and Ypixel(8 downto 4) = 8 else
+      to_unsigned(5, 5)              when Xpixel(8 downto 4) = 2 and Ypixel(8 downto 4) = 9 else
+      "0" & highscore_lut(4)(1)      when Xpixel(8 downto 4) = 5 and Ypixel(8 downto 4) = 9 else
+      "0" & highscore_lut(4)(2)      when Xpixel(8 downto 4) = 6 and Ypixel(8 downto 4) = 9 else
+      "0" & highscore_lut(4)(3)      when Xpixel(8 downto 4) = 7 and Ypixel(8 downto 4) = 9 else
+      "0" & highscore_lut(4)(4)      when Xpixel(8 downto 4) = 8 and Ypixel(8 downto 4) = 9 else
+      "0" & highscore_lut(4)(5)      when Xpixel(8 downto 4) = 9 and Ypixel(8 downto 4) = 9 else
+      "0" & highscore_lut(4)(6)      when Xpixel(8 downto 4) = 10 and Ypixel(8 downto 4) = 9 else
+      to_unsigned(19, 5);
+  
 
   process(clk) begin
     if rising_edge(clk) then
@@ -285,12 +342,13 @@ begin
     data_out => highScore_data);
 
   -- Tile memory
-  process(clk) begin
-    if rising_edge(clk) then
-      if rst = '1' then
+process(clk) begin
+  if rising_edge(clk) then
+    if rst = '1' then
         Xpepe <= "0110101000";
         Ypepe <= "0111000000";
-      elsif dead = '0' then
+    else
+      if dead = '0' then
         if counter = 0 then
           if move_pepe_in = "010" and Xpepe < "1001011111" then
               Xpepe <= Xpepe + 1;
@@ -302,85 +360,77 @@ begin
               Ypepe <= Ypepe - 1;
           end if;
         end if;
-        if (blank = '0') then
-          if (Xpixel < 240 and Ypixel < 16) then
-            -- Highscore område
-            tilePixel <= highScore_data;
-          elsif (Xpixel > 239) then
+      end if;
+      if (blank = '0') then
+        if (Xpixel < 240) then
+           tilePixel <= highScore_data;
+        elsif dead = '0' then
             -- Spelplan
-            if (Xpixel >= Xpepe and Xpixel < Xpepe + 32) and (Ypixel >= Ypepe and Ypixel < Ypepe + 32) then
-              if sprite_data = "00000000" then
-                tilePixel <= data;
-              elsif data = "00000000" then
+          if (Xpixel >= Xpepe and Xpixel < Xpepe + 32) and (Ypixel >= Ypepe and Ypixel < Ypepe + 32) then
+            if sprite_data = "00000000" then
+              tilePixel <= data;
+            elsif data = "00000000" then
                 tilePixel <= sprite_data;
-              else
-                if (Ypixel - Ypepe) = 17 then
+            else
+              if (Ypixel - Ypepe) = 17 then
+                case move_pepe_in is
+                  when "010" => Xpepe <= Xpepe - 1;
+                  when "001" => Xpepe <= Xpepe + 1;
+                  when others => Ypepe <= Ypepe + 1;
+                end case;
+              elsif (Ypixel - Ypepe) < 17 then
+                if Ypixel - Ypepe < 13 and Ypixel - Ypepe > 5 then
                   case move_pepe_in is
                     when "010" => Xpepe <= Xpepe - 1;
                     when "001" => Xpepe <= Xpepe + 1;
-                    when others => Ypepe <= Ypepe + 1;
+                    when others => null;
                   end case;
-                elsif (Ypixel - Ypepe) < 17 then
-                  if Ypixel - Ypepe < 13 and Ypixel - Ypepe > 5 then
-                    case move_pepe_in is
-                      when "010" => Xpepe <= Xpepe - 1;
-                      when "001" => Xpepe <= Xpepe + 1;
-                      when others => null;
-                    end case;
-                  else
-                    Ypepe <= Ypepe + 1;
-                    if (Xpixel - Xpepe < 13) then
-                      Xpepe <= Xpepe + 1;
-                    elsif (Xpixel - Xpepe > 18) then
-                      Xpepe <= Xpepe - 1;
-                    end if;
-                  end if;
                 else
-                  if Xpixel - Xpepe < 13 then
+                  Ypepe <= Ypepe + 1;
+                  if (Xpixel - Xpepe < 13) then
                     Xpepe <= Xpepe + 1;
-                  elsif Xpixel - Xpepe > 18 then
+                  elsif (Xpixel - Xpepe > 18) then
                     Xpepe <= Xpepe - 1;
-                  else
-                    Ypepe <= Ypepe - 1;
                   end if;
                 end if;
+              else
+                if Xpixel - Xpepe < 13 then
+                  Xpepe <= Xpepe + 1;
+                elsif Xpixel - Xpepe > 18 then
+                  Xpepe <= Xpepe - 1;
+                else
+                  Ypepe <= Ypepe - 1;
+                end if;
               end if;
-            else
-              tilePixel <= data;
             end if;
+          else
+            tilePixel <= data;
+          end if;
+        
+        elsif dead = '1' then
+          if (Xpixel >= Xpepe and Xpixel < Xpepe + 32) and (Ypixel >= Ypepe and Ypixel < Ypepe + 32) then
+            if sprite_data = "00000000" then
+              tilePixel <= data;
+            elsif data = "00000000" then
+              tilePixel <= sprite_data;
+            end if;
+          elsif Ypixel > 192 and Ypixel < 241 then -- GAME
+            tile_index_d <= to_unsigned(16, 5);
+            tilePixel <= data;
+          elsif  Ypixel > 240 and  Ypixel < 288 then -- OVER
+            tile_index_d <= to_unsigned(17, 5);
+            tilePixel <= data;
+          else
+            tile_index_d <= "0" & lut(to_integer(home_cp));
+            tilePixel <= data;
           end if;
         else
           tilePixel <= (others => '0');
         end if;
-      elsif dead = '1' then
-        if (blank = '0') then
-          if (Xpixel < 240 and Ypixel < 16) then
-            -- Highscore område
-            tilePixel <= highScore_data;
-          elsif (Xpixel > 239) then
-            if (Xpixel >= Xpepe and Xpixel < Xpepe + 32) and (Ypixel >= Ypepe and Ypixel < Ypepe + 32) then
-              if sprite_data = "00000000" then
-                tilePixel <= data;
-              elsif data = "00000000" then
-                tilePixel <= sprite_data;
-              end if;
-            elsif Ypixel > 192 and Ypixel < 241 then -- GAME
-              tile_index_d <= to_unsigned(16, 5);
-              tilePixel <= data;
-            elsif  Ypixel > 240 and  Ypixel < 288 then -- OVER
-              tile_index_d <= to_unsigned(17, 5);
-              tilePixel <= data;
-            else
-              tile_index_d <= "0" & lut(to_integer(home_cp));
-              tilePixel <= data;
-            end if;
-          else
-            tilePixel <= (others => '0');
-          end if;
-        end if;
       end if;
     end if;
-  end process;
+  end if;
+end process;
 
   process(clk) begin
     if rising_edge(clk) then
@@ -388,6 +438,44 @@ begin
     end if;
   end process;
 
+
+ process(clk) begin
+    if rising_edge(clk) then
+      if rst = '1' and bajs = '0' then
+        if index_save(1) > highscore_lut(0)(1) then
+          highscore_lut <= highscore_lut(3 downto 0) & index_save;
+          bajs <= '1';
+        elsif index_save(1) = highscore_lut(0)(1) then
+          if index_save(2) > highscore_lut(0)(2) then
+            highscore_lut <= highscore_lut(3 downto 0) & index_save;
+            bajs <= '1';
+          elsif index_save(2) = highscore_lut(0)(2) then
+            if index_save(3) > highscore_lut(0)(3) then
+              highscore_lut <= highscore_lut(3 downto 0) & index_save;
+              bajs <= '1';
+            elsif index_save(3) = highscore_lut(0)(3) then
+              if index_save(4) > highscore_lut(0)(4) then
+                highscore_lut <= highscore_lut(3 downto 0) & index_save;
+                bajs <= '1';
+              elsif index_save(4) = highscore_lut(0)(4) then
+                if index_save(5) > highscore_lut(0)(5) then
+                  highscore_lut <= highscore_lut(3 downto 0) & index_save;
+                  bajs <= '1';
+                elsif index_save(5) = highscore_lut(0)(5) then
+                  if index_save(6) >= highscore_lut(0)(6) then
+                    highscore_lut <= highscore_lut(3 downto 0) & index_save;
+                    bajs <= '1';
+                  end if;
+                end if;
+              end if;
+            end if;
+          end if;
+        end if;
+      elsif rst = '0' then
+        bajs <= '0';
+      end if;
+    end if;
+  end process;
   -- VGA generation
   vgaRed(2)     <= tilePixel(7);
   vgaRed(1)     <= tilePixel(6);
